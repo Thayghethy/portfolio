1da1c8ace9ad3ccd1a70e2f9b941160b6c149e52
Thayghethy Souza dos Santos <101431239+Thayghethy@users.noreply.github.com>
2025-03-17 00:32:46 -0300

    Deploy!

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "export",
+    images: {
+      unoptimized: true,
+    },
+    trailingSlash: true,
+  };
+  
+  export default nextConfig;
+  
